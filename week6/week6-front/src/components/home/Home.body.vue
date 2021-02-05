db547a6a57875a370f8407056afe143dd2a76374
Valentine Romantsov <valentin1998romantsov@gmail.com>
2021-02-05 11:51:52 +0200

    minor fixes

--- week6/week6-front/src/components/home/Home.body.vue
+++ week6/week6-front/src/components/home/Home.body.vue
@@ -335,7 +335,9 @@
                         required
                         class="custom-select my-1 mr-sm-2"
                         id="currency"
-                        v-model="this.$store.state.forceExchangeSelectedCurrency"
+                        v-model="
+                          this.$store.state.forceExchangeSelectedCurrency
+                        "
                       >
                         <option
                           v-for="(currency, index) in this.$store.state.userData
