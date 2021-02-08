a02f01105849107f244608178cecb378f96fb44f
Valentine Romantsov <valentin1998romantsov@gmail.com>
2021-02-08 04:01:59 +0200

    bless

--- week6/week6-front/src/axios.js
+++ week6/week6-front/src/axios.js
@@ -3,5 +3,11 @@ import axios from 'axios'
 
 axios.defaults.baseURL = "http://localhost/";
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
-
 axios.defaults.headers.common['Accept'] = 'application/json';
+
+axios.interceptors.request.use(
+	(config) => {
+		config.headers['Authorization'] = 'Bearer ' + localStorage.getItem('token');
+		return config;
+	}
+);
